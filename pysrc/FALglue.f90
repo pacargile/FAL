efd303de2248c8085d10fa4992f367b4fb90870a
Phillip Cargile <pcargile@cfa.harvard.edu>
2016-04-29 14:13:33 -0400

    edits

--- pysrc/FALglue.f90
+++ pysrc/FALglue.f90
@@ -46,10 +46,10 @@ subroutine readoutspecbin(&
   real(c_double), intent(out) :: CODEin(NLINESi)
   real(c_double), intent(out) :: Ein(NLINESi)
   real(c_double), intent(out) :: XJin(NLINESi)
-  character(kind=c_char,len=15),   intent(out) :: LABELin(NLINESi)
+  character(kind=c_char,len=40),   intent(out) :: LABELin(NLINESi)
   real(c_double), intent(out) :: EPin(NLINESi)
   real(c_double), intent(out) :: XJPin(NLINESi)
-  character(kind=c_char,len=*),   intent(out) :: LABELPin(NLINESi)
+  character(kind=c_char,len=40),   intent(out) :: LABELPin(NLINESi)
   real(c_double), intent(out) :: GRin(NLINESi)
   real(c_double), intent(out) :: DGAMMARin(NLINESi)
   real(c_double), intent(out) :: GSin(NLINESi)
@@ -88,7 +88,7 @@ subroutine readoutspecbin(&
   REAL*4 X1,X2,ELO,GF,GS,GR,GW
   REAL*4 DWL,DGFLOG,DGAMMAR,DGAMMAS,DGAMMAW,EXTRA1,EXTRA2,EXTRA3
   REAL*4 ALINEC, NELION, NBLO, NBUP, ISO1, ISO2, ISOSHIFT
-  character*15 LABEL,LABELP,REF,OTHER1,OTHER2
+  character*40 LABEL,LABELP,REF,OTHER1,OTHER2
 
   INTEGER IWL, NWL, I, NLINESO
 
