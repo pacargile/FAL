cc67ebcdeecb2eee17b7150bcbc81f918753f624
Phillip Cargile <pcargile@cfa.harvard.edu>
2016-04-29 17:10:41 -0400

    edits

--- pysrc/FALglue.f90
+++ pysrc/FALglue.f90
@@ -44,7 +44,7 @@ subroutine readoutspecbin(&
   real(c_double), intent(out) :: CODEin(NLINESi)
   real(c_double), intent(out) :: Ein(NLINESi)
   real(c_double), intent(out) :: XJin(NLINESi)
-  character(kind=c_char,len=1),  intent(out) :: LABELin(NLINESi,1)
+  character(kind=c_char,len=1),  intent(out) :: LABELin(NLINESi)
   ! real(c_double), intent(out) :: EPin(NLINESi)
   ! real(c_double), intent(out) :: XJPin(NLINESi)
   ! character(kind=c_char,len=1),   intent(out) :: LABELPin(NLINESi)
@@ -132,7 +132,8 @@ subroutine readoutspecbin(&
      Ein(I) = E
      XJin(I) = XJ
      WRITE(SLABEL,'(A8)') LABEL(1)
-     LABELin(I,1) = SLABEL
+     allocate(LABELin(I),8)
+     LABELin(I) = SLABEL
      ! EPin(I) = EP
      ! XJPin(I) = XJP
      ! LABELPin(I) = LABELP(1)
