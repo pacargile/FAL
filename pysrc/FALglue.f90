953f1a9c033b921fa816c12652732594f0667d26
Phill Cargile <p.cargile@vanderbilt.edu>
2016-05-02 15:53:09 -0400

    editting

--- pysrc/FALglue.f90
+++ pysrc/FALglue.f90
@@ -153,7 +153,7 @@ subroutine readoutspecbin(&
      XJin(I) = XJ
      WRITE(SLABEL,'(A10)') LABEL(1)
      SLABELin(I) = SLABEL//c_null_char
-     LABELin(I) = f_to_c_string(SLABEL)
+     LABELin(I) = len(f_to_c_string(SLABEL))
      IF(I.EQ.1)THEN
       print *, SLABEL
       print *, SLABELin(I)
