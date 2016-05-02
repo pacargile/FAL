b328481bb4e73256848a7be153f8370838b70221
Phill Cargile <p.cargile@vanderbilt.edu>
2016-05-01 21:59:53 -0400

    editting

--- pysrc/FALglue.f90
+++ pysrc/FALglue.f90
@@ -45,7 +45,6 @@ subroutine readoutspecbin(&
   real(c_double), intent(out) :: Ein(NLINESi)
   real(c_double), intent(out) :: XJin(NLINESi)
   character(kind=c_char,len=1),  intent(inout) :: LABELin(NLINESi)
-  allocate(character(len=8) :: LABELin(NLINESi))
   ! real(c_double), intent(out) :: EPin(NLINESi)
   ! real(c_double), intent(out) :: XJPin(NLINESi)
   ! character(kind=c_char,len=1),   intent(out) :: LABELPin(NLINESi)
@@ -118,6 +117,8 @@ subroutine readoutspecbin(&
 140     FORMAT(F11.4,F7.4,2F7.3,F8.2,F12.3,F5.1,1X,A8,A2,&
   F12.3,F5.1,1X,A8,A2,6F6.2,F11.3,&
   1X,A4,I2,I2,I3,F6.3,I3,F6.3,A8,A2,A8,A2,I6,I4,2X,f8.4)
+
+  allocate(character(len=8) :: LABELin(NLINESi))
   
   DO 9 I=1,NLINESO
      READ(1)LINDAT8,LINDAT
