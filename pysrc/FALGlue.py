707fc89c420dfb3bca0a3022f499bb849f43db7e
Phillip Cargile <pcargile@cfa.harvard.edu>
2016-05-06 12:22:01 -0400

    editting

--- pysrc/FALGlue.py
+++ pysrc/FALGlue.py
@@ -227,7 +227,7 @@ class glue(object):
 		ll['ISOSHIFT'] = ISOSHIFTin
 		ll['NELION']   = NELIONin
 		ll['OTHER'] = np.array(['{0}{1}{2:5.0f}{3:5.0f}'.format(x1,x2,x3,x4) for x1,x2,x3,x4 in zip(ll['OTHER1'],ll['OTHER2'],ll['ISOSHIFT'],ll['NELION'])])
-		ll['RESID']    = RESIDin
+		ll['RESID']    = np.array(['{0:6.3}'.format(x) for x in RESIDin],dtype='float')
 
 		return (outspec,ll)
 
