1c5e545b86d2d0bb5a1daa074a5fcb507f386dba
Phillip Cargile <pcargile@cfa.harvard.edu>
2016-06-24 12:19:43 -0400

    fixed bug in FALGlue

--- pysrc/FALGlue.py
+++ pysrc/FALGlue.py
@@ -202,11 +202,11 @@ class glue(object):
 		ll['GFLOG']    = np.array(['{0:7.3f}'.format(x) for x in GFLOGin],dtype='float')
 		ll['DGFLOG']   = np.array(['{0:7.3f}'.format(x) for x in DGFLOGin],dtype='float')
 		ll['CODE']     = np.array(['{0:8.2f}'.format(x) for x in CODEin],dtype='str')
-		ll['E']        = Ein    
-		ll['XJ']       = XJin    
+		ll['E']        = np.array(['{0:12.3f}'.format(x) for x in Ein],dtype='float')    
+		ll['XJ']       = np.array(['{0:5.1f}'.format(x) for x in XJin],dtype='float')
 		ll['LABEL']    = np.array([''.join(LABELin[i,:]) for i in range(NLINES)])
-		ll['EP']       = EPin    
-		ll['XJP']      = XJPin
+		ll['EP']       = np.array(['{0:12.3f}'.format(x) for x in EPin],dtype='float')
+		ll['XJP']      = np.array(['{0:5.1f}'.format(x) for x in XJPin],dtype='float')
 		ll['LABELP']   = np.array([''.join(LABELPin[i,:]) for i in range(NLINES)])
 		ll['GR']       = np.array(['{0:6.2f}'.format(x) for x in GRin],dtype='float')
 		ll['DGAMMAR']  = DGAMMARin
