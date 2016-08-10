173fa5c0e941dd4c82313fe96bd6cc2d0df6a878
Phillip Cargile <pcargile@cfa.harvard.edu>
2016-08-10 10:45:12 -0400

    testing

--- pysrc/FALdist.py
+++ pysrc/FALdist.py
@@ -129,7 +129,7 @@ def makeinlist(infilename):
 			'minlinWL':float(rf_i['LINWLstart']),'maxlinWL':float(rf_i['LINWLend']),
 			'arcscale':float(rf_i['ARCSCALE']),
 			'outputfile':'MCMC_{0}.dat'.format(rf_i['ID']),
-			'outputdir':'/work/02349/cargilpa/FAL/TESTING/SIMMOD/TEST13/TEST/',
+			'outputdir':'/work/02349/cargilpa/FAL/HBAND/RUN1_V3/',
 			'RUNID':ii})
 
 		indictlist.append(tempdict)
