7d356c42cdc984bb0edf3699715fc28e2ac1fb13
Phillip Cargile <pcargile@cfa.harvard.edu>
2017-01-09 11:21:39 -0500

    Made changes to work on Ody

--- pysrc/FALmod.py
+++ pysrc/FALmod.py
@@ -239,12 +239,12 @@ class FALmod(object):
             # determine which readmaster line list to use base on wavelength range
             if self.masterll == None:
                 if (self.starpars['WSTART'] > 450.0) & (self.starpars['WEND'] < 1350.0):
-                    # MASTERLL = ['/work/02349/cargilpa/FAL/MASTERLL/OPTSEG/KuruczLL_450_1350.bin']
-                    MASTERLL = ['/work/02349/cargilpa/FAL/MASTERLL/OPTSEG/CarLLRun2_450_1350.bin']
+                    # MASTERLL = ['/n/conroyfs1/pac/FAL/data/LL/KuruczLL_450_1350.bin']
+                    MASTERLL = ['/n/conroyfs1/pac/FAL/data/LL/CarLLRun2_450_1350.bin']
                 elif (self.starpars['WSTART'] > 1400.0) & (self.starpars['WEND'] < 1900.0):
-                    # MASTERLL = (['/work/02349/cargilpa/FAL/MASTERLL/HBAND_V3/RUN2/CarLL_1475_1800.bin',
-                    MASTERLL = (['/work/02349/cargilpa/FAL/MASTERLL/HBAND/KuruczLL_1400_1900.bin',
-                    '/work/02349/cargilpa/FAL/MASTERLL/HBAND/KuruczH2OLL_1400_1900.bin'])
+                    # MASTERLL = (['/n/conroyfs1/pac/FAL/data/LL/CarLLRun2_1475_1800.bin',
+                    MASTERLL = (['/n/conroyfs1/pac/FAL/data/LL/KuruczLL_1400_1900.bin',
+                    '/n/conroyfs1/pac/FAL/data/LL/KuruczH2OLL_1400_1900.bin'])
                 else:
                     raise ValueError('DID NOT UNDERSTAND MASTERLL')
             else:
