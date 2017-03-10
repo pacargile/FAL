71d0c67e48a11718287c63ad9f8c852c81115a1c
Phillip Cargile <pcargile@cfa.harvard.edu>
2017-03-10 10:38:53 -0500

    changed H-band ll to newest bin files

--- pysrc/FALdist.py
+++ pysrc/FALdist.py
@@ -136,7 +136,7 @@ def makeinlist(infilename):
 			'minlinWL':float(rf_i['LINWLstart']),'maxlinWL':float(rf_i['LINWLend']),
 			'arcscale':float(rf_i['ARCSCALE']),
 			'outputfile':'MCMC_{0}.dat'.format(rf_i['ID']),
-			'outputdir':'/n/regal/conroy_lab/pac/FAL/OPTSEG/RUN2/MCMCoutput/',
+			'outputdir':'/n/regal/conroy_lab/pac/FAL/HBAND/test/MCMCoutput/',
 			# 'initlines':None,
 			'initlines':'/n/regal/conroy_lab/pac/FAL/HBAND/CarRun1_LL_1400_1900.h5',
 			'RUNID':ii})
