05397304a762f8c84735ec23546c17f20c66dde2
Phillip Cargile <pcargile@cfa.harvard.edu>
2017-07-17 09:45:04 -0400

    added reading in TiO to readmaster

--- pysrc/FALmod.py
+++ pysrc/FALmod.py
@@ -274,7 +274,7 @@ class FALmod(object):
             self.SYNTHE.readlines(rtype='readmaster',verbose=verbose_i,MASTERLL=MASTERLL)
             self.speed = 'slow'
             if self.timeit:
-                print("Pro: {1} --> Read in master line list: {2} -- Step time: {0:7.5f} s".format(time.time()-self.lasttime,self.IDraw,MASTERLL[0]))
+                print("Pro: {1} --> Read in master line list: {2} -- Step time: {0:7.5f} s".format(time.time()-self.lasttime,self.IDraw,MASTERLL))
                 self.lasttime = time.time()
             return
 
