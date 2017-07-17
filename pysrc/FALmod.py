de62a2f351fcaf7412aefc34e88799cec0292418
Phillip Cargile <pcargile@cfa.harvard.edu>
2017-07-17 09:54:32 -0400

    added predicted lines to readall

--- pysrc/FALmod.py
+++ pysrc/FALmod.py
@@ -229,8 +229,8 @@ class FALmod(object):
 
         elif linelist == 'readall':
             # read all individual line lists
-            rlinedict = {"TiO":True} # atoms, molecules + H2O & TiO
-            # rlinedict = {"atoms":True,"moles":True,"H2O":True,"TiO":True,"predict":True} # atoms, molecules + H2O & TiO
+            # rlinedict = {"TiO":True} # atoms, molecules + H2O & TiO
+            rlinedict = {"atoms":True,"moles":True,"H2O":True,"TiO":False,"predict":True} # atoms, molecules + H2O & TiO
             self.SYNTHE.readlines(rtype='readall',rlinedict=rlinedict,verbose=verbose_i)
             self.speed = 'slow'
 
