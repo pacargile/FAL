3f63b26b96f2d471d4ed3d440677c22bc786b107
Phillip Cargile <pcargile@cfa.harvard.edu>
2017-07-18 13:22:51 -0400

    added predicted lines to readall

--- pysrc/FALmod.py
+++ pysrc/FALmod.py
@@ -243,7 +243,7 @@ class FALmod(object):
             # determine which readmaster line list to use base on wavelength range
             if self.masterll == None:
                 if (self.starpars['WSTART'] > 450.0) & (self.starpars['WEND'] < 1300.0):
-                    MASTERLL = (['/n/conroyfs1/pac/MASTERLL/OPTSEG/LL/Kur_LL_450_1300_CLEAN.bin',
+                    MASTERLL = (['/n/conroyfs1/pac/MASTERLL/FULLOPT/LL/Kur_LL_450_1300_CLEAN.bin',
                         '/n/conroyfs1/pac/MASTERLL/TiO/TiO_LL_450_1300.bin'])
                     # MASTERLL = ['/n/conroyfs1/pac/MASTERLL/FULLOPT/CR2_LL_475_1000.bin']
                 # elif (self.starpars['WSTART'] > 845.0) & (self.starpars['WEND'] < 1350.0):
