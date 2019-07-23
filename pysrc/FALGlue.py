1941d2e170a7cdacccedd565b6f79b1ffa94b3ff
Phillip Cargile <pcargile@cfa.harvard.edu>
2019-07-23 15:56:00 -0400

    bug fixing

--- pysrc/FALGlue.py
+++ pysrc/FALGlue.py
@@ -137,7 +137,7 @@ class glue(object):
 	def readspecbin(self,filename,NWL=int(12000),NLINES=int(1000000)):
 
 		s_i  = '{0}'.format(filename)
-		s    = s_i
+		s    = bytes(s_i)
 		SWL   = np.zeros(NWL, dtype="double")
 		QMU1 = np.zeros(NWL, dtype="double")
 		QMU2 = np.zeros(NWL, dtype="double")
