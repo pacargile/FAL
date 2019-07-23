b13f3dd7e32f82685b5fc5e668618c71d72ebe6d
Phillip Cargile <pcargile@cfa.harvard.edu>
2019-07-23 14:03:10 -0400

    bug fixing

--- pysrc/FALsynthepkg.py
+++ pysrc/FALsynthepkg.py
@@ -492,21 +492,23 @@ class synthe(object):
         """
         Run read line code to manually read in all data
         """
-
         try:
             if rlinedict['atoms']==True:
+                print('... Reading ATOMS')
                 # do atomic lines
                 self.ratomic(verbose=verbose)
         except KeyError:
             pass
         try:
             if rlinedict['predict']==True:
+                print('... Reading PRED')
                 # do predicted lines
                 self.readpredlines(verbose=verbose)
         except KeyError:
             pass
         try:
             if rlinedict['moles']==True:
+                print('... Reading MOLECULES')
                 self.molefiles = ([
                     'alopatrascu.asc', # AlO
                     'nah.dat', # NaH
@@ -542,6 +544,7 @@ class synthe(object):
             pass
         try:
             if rlinedict['TiO']==True:
+                print('... Reading TiO')
                 # do TiO lines
                 self.readmol_TiO(verbose=verbose)
             if os.path.isfile("fort.48"):
@@ -556,6 +559,7 @@ class synthe(object):
             pass
         try:
             if rlinedict['H2O']==True:
+                print('... Reading H2O')
                 # do H20 lines
                 self.readmol_H2O(verbose=verbose)
             if os.path.isfile("fort.48"):
@@ -571,6 +575,7 @@ class synthe(object):
         
         try:
             if rlinedict['injectlines'] != None:
+                print('... Reading INJECTED LINES')
                 # inject lines into line list
                 self.rinjectlines(verbose=verbose,injectll=rlinedict['injectlines'])
         except KeyError:
