bbbb3ef644c0c1927de54f3e27a987c4a1901c1e
pacargile <pcargile@cfa.harvard.edu>
2019-07-23 21:38:00 -0400

    bug fixing

--- pysrc/FALGlue.py
+++ pysrc/FALGlue.py
@@ -220,8 +220,8 @@ class glue(object):
 		# QMU1 = np.trim_zeros(QMU1,trim='b')
 		# QMU2 = np.trim_zeros(QMU2,trim='b')
 
-		print(LABELin[0,:].tostring('C').decode('ascii'))
-		print(len(LABELin[0,:].tostring('C').decode('ascii')))
+		print(LABELin[0,:].tostring('F').decode('ascii'))
+		print(len(LABELin[0,:].tostring('F').decode('ascii')))
 		print(''.join(LABELin[0,:].tostring('F').decode('ascii')))
 
 		outspec = {'WAVE':SWL,'QMU1':QMU1,'QMU2':QMU2}
