ef0666920924c2a62c5fec456d53ba56e00ad37c
Phillip Cargile <pcargile@cfa.harvard.edu>
2019-07-25 14:12:26 -0400

    bug fix

--- pysrc/FALglue.f90
+++ pysrc/FALglue.f90
@@ -166,14 +166,14 @@ subroutine readoutspecbin(&
      ! END DO
      Ein(I) = E
      XJin(I) = XJ
-     WRITE(SLABEL,' (A8,A2)') LABEL
+     WRITE(SLABEL,'(A8,A2)') LABEL
      SLABEL = SLABEL//c_null_char
      DO J=0,10
        LABELin(J,I) = SLABEL(J:J)
      END DO
      EPin(I) = EP
      XJPin(I) = XJP
-     WRITE(SLABEL,' (A8,A2)') LABELP
+     WRITE(SLABEL,'(A8,A2)') LABELP
      SLABEL = SLABEL//c_null_char
      DO J=0,10
        LABELPin(J,I) = SLABEL(J:J)
@@ -185,7 +185,7 @@ subroutine readoutspecbin(&
      GWin(I) = GW
      DGAMMAWin(I) = DGAMMAW
      WAVENOin(I) = WAVENO
-     WRITE(SLABEL,' (A4)') REF
+     WRITE(SLABEL,'(A4)') REF
      SLABEL = SLABEL//c_null_char
      DO J=0,5
        REFin(J,I) = SLABEL(J:J)
