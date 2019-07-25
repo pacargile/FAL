4d104671bd5e4bcbd73918ad9683d69603ca2c47
Phillip Cargile <pcargile@cfa.harvard.edu>
2019-07-25 16:31:06 -0400

    bug fix

--- pysrc/FALGlue.py
+++ pysrc/FALGlue.py
@@ -299,7 +299,7 @@ class glue(object):
 		# ll['OTHER'] = np.array(
 		# 	['{0}{1}{2:6.0f}{3:4.0f}'.format(x1,x2,x3,x4) for x1,x2,x3,x4 in zip(
 		# 		ll['OTHER1'],ll['OTHER2'],ll['ISOSHIFT'],ll['NELION'])])
-		ll['RESID']    = np.array(['{0:8.4f}'.format(x) for x in RESIDin],dtype='float')
+		ll['RESID']    = np.array(['  {0:8.4f}'.format(x) for x in RESIDin],dtype='float')
 
 		sortind = np.argsort(ll['WL'])
 
