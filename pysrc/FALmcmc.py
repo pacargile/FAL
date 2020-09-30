3028102e5bcc21d6f365c8d2e16d8725db4f2ebe
pacargile <pcargile@cfa.harvard.edu>
2020-09-30 13:00:12 -0400

    updating code

--- pysrc/FALmcmc.py
+++ pysrc/FALmcmc.py
@@ -29,7 +29,7 @@ def lnprob(pin,args,verbose=False,justprior=False):
     # read in arguments
     (solobswave,solobsflux,arcobswave,arcobsflux,transflux,bg_sol_flux,bg_arc_flux,fmdict,fmll,Tarr,minWL,maxWL,minLWL,maxLWL) = args
     # for printing to command line, generate an ID
-    ID = fmdict.keys()[0] - 10000000
+    ID = list(fmdict.keys())[0] - 10000000
 
     # set up observed data dictionaries
     obswave = {'Sun':solobswave,'Arcturus':arcobswave}
