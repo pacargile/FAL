0d7c2d5fc1b75531dcdcad6dd83f8000e1ec6f68
pacargile <pcargile@cfa.harvard.edu>
2020-10-05 14:33:51 -0400

    updating code

--- pysrc/FALopt.py
+++ pysrc/FALopt.py
@@ -542,6 +542,7 @@ class FALopt(object):
 
         p0 = np.zeros(len(self.parr),dtype='float')
 
-        res = minimize(fmin, p0, method='Nelder-Mead', tol=1e-6)
+        res = minimize(fmin, p0, method='Nelder-Mead', 
+            tol=1e-6, options={'gtol': 1e-6, 'disp': True})
 
         return res
