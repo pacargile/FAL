dddf6d5eaeb73ae9be5c6e673bfa668ed6628dae
pacargile <pcargile@cfa.harvard.edu>
2020-11-02 12:04:43 -0500

    getting nested sampler working

--- pysrc/FALnest.py
+++ pysrc/FALnest.py
@@ -154,16 +154,11 @@ def priortrans(upars,args):
         if pflag_i == 'GW':
             pars_i = (0.65 - -1.5)*upars_i + -1.5
 
-        print(pflag_i,pars_i)
-        pars.append(pars_i)
-
     pars.append( (1.05 - 0.95) *upars[-4] + 0.95)
     pars.append( (1.0  - -1.0) *upars[-3] + -1.0)
     pars.append( (1.25 -  0.0) *upars[-2] + 0.0)
     pars.append( (1.0  - -1.0) *upars[-1] + -1.0)
 
-    print(pars[-4:])
-    print('')
     return pars
 
 class FALnest(object):
